11b428116948189f8720f3ac60039c9daf4435ff
Anna Ovtšinnikova <51678512+annaovtsinnikova@users.noreply.github.com>
2023-03-30 19:28:15 +0300

    Update Library.java

--- Library.java
+++ Library.java
@@ -129,54 +129,47 @@ public class Library {
     }
 
 
-    public static String evidence(boolean ifEvidenceFromTheKiller) {
+    public static String evidence(boolean ifEvidenceFromTheKiller, String causeOfDeath) {
         String evidence = "";
-        for (String s : causesOfDeath) {
-            if (ifEvidenceFromTheKiller) {
-                switch (s) {
-                    case "was poisoned" -> evidence = "the bottle of cyanide was found in person's possession";
-                    case "was shot dead" ->
-                            evidence = "Browning 1911-22, which was used to kill, belongs to the person";
-                    case "got a drug overdose" ->
-                            evidence = "fingerprints on the 'Cymbalta' bottle, antidepressants, the overdose of which caused death";
-                    case "was hit by a car" ->
-                            evidence = "the person eas caught on camera at the gas station with the car that a witness saw at the crime scene.";
-                    case "fell or was pushed off the bridge was drowned in the river" ->
-                            evidence = "the shoe of the victim was found in person's possession";
-                    case "was drowned in the bath" ->
-                            evidence = "there was a fight and person's dna was found under the victim's fingernails";
-                    case "got stabbed" -> evidence = "a knife with victim's blood was found at person's house";
-                    case "was buried alive" ->
-                            evidence = "person's hair and blood drops with the same blood type were found on the victim";
-                    case "blunt force trauma to the head" -> evidence = "baseball bat with victim's blood";
-                    case "was robbed and beaten" -> evidence = "victim's possessions found at person's house";
-                    case "was found hanged in his/her flat" ->
-                            evidence = "clothing fibers and a partial shoe print found at the crime scene that belonged to that person";
-                    case "was suffocated" ->
-                            evidence = "the scarf, with which the crime was committed, was found in person's possession";
-                }
-            } else {
-                switch (s) {
-                    case "was poisoned" -> evidence = "person has been working with chemicals for a while";
-                    case "was shot dead" -> evidence = "person sent a threatening message to the victim 2 weeks ago";
-                    case "got a drug overdose" -> evidence = "the person was a drug addict in the past";
-                    case "was hit by a car" ->
-                            evidence = "a witness saw a car with a license plate number that belongs to the person";
-                    case "fell or was pushed off the bridge was drowned in the river" ->
-                            evidence = "the person works close to the crime scene";
-                    case "was drowned in the bath", "was suffocated" ->
-                            evidence = "person's fingerprints were found at victim's house";
-                    case "got stabbed" ->
-                            evidence = "the person had a meeting with the victim planned on the day of the murder";
-                    case "was buried alive" ->
-                            evidence = "a witness claimed that has seen somebody who looked like the person";
-                    case "blunt force trauma to the head" -> evidence = "the person is a baseball player";
-                    case "was robbed and beaten" -> evidence = "the person really needed money";
-                    case "was found hanged in his/her flat" ->
-                            evidence = "the person had recently had an argument with the victim";
-                }
-
+        if (ifEvidenceFromTheKiller) {
+            switch (causeOfDeath) {
+                case "was poisoned" -> evidence = "the bottle of cyanide was found in person's possession";
+                case "was shot dead" -> evidence = "Browning 1911-22, which was used to kill, belongs to the person";
+                case "got a drug overdose" ->
+                        evidence = "fingerprints on the 'Cymbalta' bottle, antidepressants, the overdose of which caused death";
+                case "was hit by a car" ->
+                        evidence = "the person was caught on camera at the gas station with the car that a witness saw at the crime scene.";
+                case "fell or was pushed off the bridge was drowned in the river" ->
+                        evidence = "the shoe of the victim was found in person's possession";
+                case "was drowned in the bath" ->
+                        evidence = "there was a fight and person's dna was found under the victim's fingernails";
+                case "got stabbed" -> evidence = "a knife with victim's blood was found at person's house";
+                case "was buried alive" ->
+                        evidence = "person's hair and blood drops with the same blood type were found on the victim";
+                case "blunt force trauma to the head" -> evidence = "baseball bat with victim's blood";
+                case "was robbed and beaten" -> evidence = "victim's possessions found at person's house";
+                case "was found hanged in his/her flat" ->
+                        evidence = "clothing fibers and a partial shoe print found at the crime scene that belonged to that person";
+                case "was suffocated" ->
+                        evidence = "the scarf, with which the crime was committed, was found in person's possession";
             }
+        } else {
+            evidence = switch (causeOfDeath) {
+                case "was poisoned" -> "person has been working with chemicals for a while";
+                case "was shot dead" -> "person has sent a threatening message to the victim 2 weeks ago";
+                case "got a drug overdose" -> "the person was a drug addict in the past";
+                case "was hit by a car" -> "a witness saw a car with a license plate number that belongs to the person";
+                case "fell or was pushed off the bridge was drowned in the river" ->
+                        "the person works close to the crime scene";
+                case "was drowned in the bath", "was suffocated" ->
+                        "person's fingerprints were found at victim's house";
+                case "got stabbed" -> "the person had a meeting with the victim planned on the day of the murder";
+                case "was buried alive" -> "a witness claimed that has seen somebody who looked like that person";
+                case "blunt force trauma to the head" -> "the person is a baseball player";
+                case "was robbed and beaten" -> "the person really needed money";
+                case "was found hanged in his/her flat" -> "the person had recently had an argument with the victim";
+                default -> evidence;
+            };
         }
         return evidence;
     }
