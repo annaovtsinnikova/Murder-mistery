5ae5963a91686e69783557474d35502bd1338f67
Anna Ovtšinnikova <51678512+annaovtsinnikova@users.noreply.github.com>
2023-03-30 15:21:04 +0300

    Update catchTheKiller.java

--- catchTheKiller.java
+++ catchTheKiller.java
@@ -1,112 +1,87 @@
 package OOP.catchTheKiller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class catchTheKiller extends Library{
 
     static List<Suspect> suspects = new ArrayList<>();
-    static List<String> namesUsed = new ArrayList<>();
+    static List<String> namesNotUsed = new ArrayList<>(Arrays.asList(getNames()));
+    static Random random = new Random();
+
+    public static String getRandomUniqueName() {
+        String name = namesNotUsed.get(random.nextInt(namesNotUsed.size()));
+        namesNotUsed.remove(name);
+        return name;
+    }
 
     public static Investigator whoIsOurInvestigator() {
-        Random random = new Random();
-        int randomIndex1 = random.nextInt(getNames().length);
-        return new Investigator(getNames()[randomIndex1], random.nextInt(60 - 25) + 25);
+        return new Investigator(getRandomUniqueName(), random.nextInt(60 - 25) + 25);
     }
 
     public static Victim whoWasKilled() {
-        Random random = new Random();
         Victim victim;
-        do {
-            int randomIndex1 = random.nextInt(getNames().length);
-            int randomIndex2 = random.nextInt(getOccupation().length);
-            int randomIndex3 = random.nextInt(getCausesOfDeath().length);
-            victim = new Victim(getNames()[randomIndex1], random.nextInt(95 - 16) + 16,
-                    getOccupation()[randomIndex2], getCausesOfDeath()[randomIndex3]);
-            namesUsed.add(victim.getName());
-        } while (namesUsed.contains(victim.getName()));
-
+        int randomIndex1 = random.nextInt(getOccupation().length);
+        int randomIndex2 = random.nextInt(getCausesOfDeath().length);
+        victim = new Victim(getRandomUniqueName(), random.nextInt(95 - 16) + 16,
+                getOccupation()[randomIndex1], getCausesOfDeath()[randomIndex2]);
         return victim;
     }
 
     //one of the suspects is the killer
     public static Suspect whoIsTheKiller() {
-        Random random = new Random();
         Suspect killer;
-        do {
-            int randomIndex1 = random.nextInt(getNames().length);
-            int randomIndex2 = random.nextInt(getOccupation().length);
-            int randomIndex3 = random.nextInt(getAlibi().length);
-            int randomIndex4 = random.nextInt(getMurderMotive().length);
-            int randomIndex5 = random.nextInt(getSuspiciousCharacterTraits().length);
-            killer = new Suspect(getNames()[randomIndex1], random.nextInt(95 - 16) + 16,
-                    getOccupation()[randomIndex2], getAlibi()[randomIndex3], false, evidence(true),
-                    true, getMurderMotive()[randomIndex4], getSuspiciousCharacterTraits()[randomIndex5]);
-            namesUsed.add(killer.getName());
-        } while (namesUsed.contains(killer.getName()));
-
+        int randomIndex1 = random.nextInt(getOccupation().length);
+        int randomIndex2 = random.nextInt(getAlibi().length);
+        int randomIndex3 = random.nextInt(getMurderMotive().length);
+        int randomIndex4 = random.nextInt(getSuspiciousCharacterTraits().length);
+        killer = new Suspect(getRandomUniqueName(), random.nextInt(95 - 16) + 16, getOccupation()[randomIndex1],
+                getAlibi()[randomIndex2], false, evidence(true), true,
+                getMurderMotive()[randomIndex3], getSuspiciousCharacterTraits()[randomIndex4]);
         return killer;
     }
 
     public static List<Suspect> getSuspects() {
-        Random random = new Random();
         Suspect suspect;
         //a suspect, who has some evidence against him and had some suspicious behaviour during the interrogation, but is not the killer
-        do {
-            int randomIndex1 = random.nextInt(getNames().length);
-            int randomIndex2 = random.nextInt(getOccupation().length);
-            int randomIndex3 = random.nextInt(getAlibi().length);
-            int randomIndex4 = random.nextInt(getSuspiciousCharacterTraits().length);
-            suspect = new Suspect(getNames()[randomIndex1], random.nextInt(95 - 16) + 16,
-                    getOccupation()[randomIndex2], getAlibi()[randomIndex3], true,
-                    evidence(false), false, null,
-                    getSuspiciousCharacterTraits()[randomIndex4]);
-            namesUsed.add(suspect.getName());
-            suspects.add(suspect);
-        } while (namesUsed.contains(suspect.getName()));
+        int randomIndex1 = random.nextInt(getOccupation().length);
+        int randomIndex2 = random.nextInt(getAlibi().length);
+        int randomIndex3 = random.nextInt(getSuspiciousCharacterTraits().length);
+        suspect = new Suspect(getRandomUniqueName(), random.nextInt(95 - 16) + 16, getOccupation()[randomIndex1],
+                getAlibi()[randomIndex2], true, evidence(false), false,
+                null, getSuspiciousCharacterTraits()[randomIndex3]);
+        suspects.add(suspect);
 
-        //a suspect, who lied about his alibi and has a murder motive, but is not the killer
-        do {
-            int randomIndex1 = random.nextInt(getNames().length);
-            int randomIndex2 = random.nextInt(getOccupation().length);
-            int randomIndex3 = random.nextInt(getAlibi().length);
-            int randomIndex4 = random.nextInt(getMurderMotive().length);
-            int randomIndex5 = random.nextInt(getPositiveCharacterTraits().length);
-            suspect = new Suspect(getNames()[randomIndex1], random.nextInt(95 - 16) + 16,
-                    getOccupation()[randomIndex2], getAlibi()[randomIndex3], false, null,
-                    false, getMurderMotive()[randomIndex4], getPositiveCharacterTraits()[randomIndex5]);
-            namesUsed.add(suspect.getName());
-            suspects.add(suspect);
-        } while (namesUsed.contains(suspect.getName()));
+        //a suspect, who has lied about his alibi and has a murder motive, but is not the killer
+        int randomIndex4 = random.nextInt(getOccupation().length);
+        int randomIndex5 = random.nextInt(getAlibi().length);
+        int randomIndex6 = random.nextInt(getMurderMotive().length);
+        int randomIndex7 = random.nextInt(getPositiveCharacterTraits().length);
+        suspect = new Suspect(getRandomUniqueName(), random.nextInt(95 - 16) + 16, getOccupation()[randomIndex4],
+                getAlibi()[randomIndex5], false, null, false,
+                getMurderMotive()[randomIndex6], getPositiveCharacterTraits()[randomIndex7]);
+        suspects.add(suspect);
 
-        //a suspect, that has some evidence against him and lied about his alibi, but is not the killer
-        do {
-            int randomIndex1 = random.nextInt(getNames().length);
-            int randomIndex2 = random.nextInt(getOccupation().length);
-            int randomIndex3 = random.nextInt(getAlibi().length);
-            int randomIndex4 = random.nextInt(getPositiveCharacterTraits().length);
-            suspect = new Suspect(getNames()[randomIndex1], random.nextInt(95 - 16) + 16,
-                    getOccupation()[randomIndex2], getAlibi()[randomIndex3], false,
-                    evidence(false), false, null,
-                    getPositiveCharacterTraits()[randomIndex4]);
-            namesUsed.add(suspect.getName());
-            suspects.add(suspect);
-        } while (namesUsed.contains(suspect.getName()));
+        //a suspect, that has some evidence against him and has lied about his alibi, but is not the killer
+        int randomIndex8 = random.nextInt(getOccupation().length);
+        int randomIndex9 = random.nextInt(getAlibi().length);
+        int randomIndex10 = random.nextInt(getPositiveCharacterTraits().length);
+        suspect = new Suspect(getRandomUniqueName(), random.nextInt(95 - 16) + 16, getOccupation()[randomIndex8],
+                getAlibi()[randomIndex9], false, evidence(false), false,
+                null, getPositiveCharacterTraits()[randomIndex10]);
+        suspects.add(suspect);
 
         //a suspect, that has a murder motive and had some suspicious behaviour during the interrogation, but is not the killer
-        do {
-            int randomIndex1 = random.nextInt(getNames().length);
-            int randomIndex2 = random.nextInt(getOccupation().length);
-            int randomIndex3 = random.nextInt(getAlibi().length);
-            int randomIndex4 = random.nextInt(getMurderMotive().length);
-            int randomIndex5 = random.nextInt(getSuspiciousCharacterTraits().length);
-            suspect = new Suspect(getNames()[randomIndex1], random.nextInt(95 - 16) + 16,
-                    getOccupation()[randomIndex2], getAlibi()[randomIndex3], true, null,
-                    false, getMurderMotive()[randomIndex4], getSuspiciousCharacterTraits()[randomIndex5]);
-            namesUsed.add(suspect.getName());
-            suspects.add(suspect);
-        } while (namesUsed.contains(suspect.getName()));
+        int randomIndex11 = random.nextInt(getOccupation().length);
+        int randomIndex12 = random.nextInt(getAlibi().length);
+        int randomIndex13 = random.nextInt(getMurderMotive().length);
+        int randomIndex14 = random.nextInt(getSuspiciousCharacterTraits().length);
+        suspect = new Suspect(getRandomUniqueName(), random.nextInt(95 - 16) + 16, getOccupation()[randomIndex11],
+                getAlibi()[randomIndex12], true, null, false,
+                getMurderMotive()[randomIndex13], getSuspiciousCharacterTraits()[randomIndex14]);
+        suspects.add(suspect);
 
         return suspects;
     }
